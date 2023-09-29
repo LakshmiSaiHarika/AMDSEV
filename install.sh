c33200787cb7505f552c258f01ebf83c3ad17341
Harika <lnittala@amd.com>
2023-09-29 13:51:37 -0700

    RHEL fix for virt group and kernel package for installation
    virt group package not found in RHEL.qemu-kvm is a
    package in RHEL for qemu
    Installing only kernel-host package resolved the issue can't install
    both kernel-host an kernel-guest package at the same time.
    
    Signed-off-by: Harika <lnittala@amd.com>

--- install.sh
+++ install.sh
@@ -6,13 +6,17 @@
 if [ "$ID" = "debian" ] || [ "$ID_LIKE" = "debian" ]; then
 	apt-get -y install qemu ovmf
 else
-	dnf install @virt edk2-ovmf -y
+
+	#dnf install @virt edk2-ovmf -y
+	dnf install qemu-kvm edk2-ovmf -y
+        
 fi
 
 if [ "$ID" = "debian" ] || [ "$ID_LIKE" = "debian" ]; then
 	dpkg -i linux/host/linux-image-*.deb
 else
-	dnf install -y linux/host/kernel-*.rpm
+	#dnf install -y linux/host/kernel-*.rpm
+	dnf install -y linux/kernel-[0-9]*host*.rpm
 fi
 
 cp kvm.conf /etc/modprobe.d/
